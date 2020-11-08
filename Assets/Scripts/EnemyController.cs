b91befbfb2637c803e4cfb636e24795c1d4e72bf
mkasza99 <mathu00000@hotmail.com>
2020-11-08 13:47:58 -0500

    first commit

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public ParticleSystem smokeEffect;
+
+    public float speed;
+    public bool vertical;
+    public float changeTime = 3.0f;
+
+    Rigidbody2D rigidbody2D;
+    float timer;
+    int direction = 1;
+    bool broken = true;
+
+    Animator animator;
+
+
+    void Start()
+    {
+       rigidbody2D = GetComponent<Rigidbody2D>(); 
+       timer = changeTime;
+       animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        timer -= Time.deltaTime;
+
+        if(!broken)
+        {
+            return;
+        }
+
+        if (timer < 0)
+        {
+            direction = -direction;
+            timer = changeTime;
+        }
+
+    }
+
+  
+    void FixedUpdate()
+    {
+        Vector2 position = rigidbody2D.position;
+
+        if(!broken)
+        {
+            return;
+        }
+
+        if (vertical)
+        {
+            position.y = position.y +Time.deltaTime * speed * direction;
+            animator.SetFloat("Move X", 0);
+            animator.SetFloat("Move Y", direction);
+        }
+        else
+        {
+            position.x = position.x + Time.deltaTime * speed * direction;
+            animator.SetFloat("Move X", direction);
+            animator.SetFloat("Move Y", 0);          
+        }
+        
+        rigidbody2D.MovePosition(position);
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        RubyController player = other.gameObject.GetComponent<RubyController>();
+
+        if (player != null)
+        {
+            player.ChangeHealth(-1);
+        }
+    }
+
+    public void Fix()
+    {
+        broken = false;
+        rigidbody2D.simulated = false;
+
+        smokeEffect.Stop();
+        Destroy(smokeEffect.gameObject);
+    }
+}
